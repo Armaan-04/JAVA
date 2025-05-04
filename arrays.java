65654ed0726f23be3298efab6d98661f3c510253
Armaan-04 <alamarmaan2004@gmail.com>
2025-05-05 02:44:35 +0530

    Create arrays.java

---
+++ arrays.java
@@ -0,0 +1,17 @@
+
+import java.util.Arrays;
+
+public class arrays{
+    public static void main(String[] args) {
+        int[] marks = new int[] {20,10,40,50};
+        System.out.println(marks[2]);
+        Arrays.sort(marks);
+        System.out.println(Arrays.toString(marks));
+
+
+        int[][] NewMarks = new int[][] {{50,55},{90,30}};
+        System.out.println(NewMarks[0][1]); //0th position and then 1st position.
+        System.out.println(Arrays.deepToString(NewMarks));
+        
+        }
+    }
