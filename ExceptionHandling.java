73d5018bb6981865d5b4cb313a3dfbf2d8604509
Armaan-04 <alamarmaan2004@gmail.com>
2025-05-16 12:58:41 +0530

    oops

--- ExceptionHandling.java
+++ ExceptionHandling.java
@@ -7,7 +7,7 @@ public class ExceptionHandling {
         int b = scanner.nextInt();
         try{
             System.out.println("division is: " + (a/b));
-        } catch (ArithmeticException e){ //first executes the block in inside try and then if an exception arises , it is shifted to catch block
+        } catch (ArithmeticException e){ //first executes the block inside try and then if an exception arises , it is shifted to catch block
             System.out.println("Division is not possible");
         } finally {//finally keyword is always executed no matter the try and catch block is executed or not.
             System.out.println("I am in finally");
