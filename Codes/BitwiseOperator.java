f0a6f6ca51c1fe8a959b216096595b78ac1d01e5
Armaan-04 <alamarmaan2004@gmail.com>
2025-05-20 10:20:14 +0530

    java

---
+++ Codes/BitwiseOperator.java
@@ -0,0 +1,20 @@
+package Codes;
+import java.util.Scanner;
+
+//Check if the number is even or odd using bitwise operator.
+//Even number bits end with 0 and Odd number bits end with 1.
+
+public class BitwiseOperator {
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter your number: ");
+        int num = input.nextInt();
+
+        if((num & 1) ==1){
+            System.out.println("Your number is odd.");
+        } else {
+            System.out.println("Your number is even.");
+        }
+
+    }
+}
---
+++ Codes/PositiveNegativeOrZero.java
@@ -0,0 +1,18 @@
+package Codes;
+import java.util.Scanner;
+
+public class PositiveNegativeOrZero {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the number");
+        int num=input.nextInt();
+
+        if (num>0){
+            System.out.println("The number is positive");
+        } else if (num == 0) {
+            System.out.println("The number is zero");
+        } else{
+            System.out.println("The number is negative");
+        }
+    }
+}
