b9bff503dd5849bdeb4ca3404dd7e325b317da12
Armaan-04 <alamarmaan2004@gmail.com>
2025-05-25 12:15:47 +0530

    arrays

---
+++ Codes/Intermediate/MaxnMin.java
@@ -0,0 +1,22 @@
+package Codes.Intermediate;
+import java.util.Scanner;
+
+public class MaxnMin {
+    public static void main(String[] args) {
+        int[] numArr = ArrayUtility.inputArray();
+        int max = max(numArr);
+        int min = min(numArr);
+        System.out.println("Max of the array is: " + max);
+        System.out.println("Min of the array is: " + min);
+    }
+
+    public static int min(int[] numArr){
+        System.out.println("Min of the array is: ");
+        return 0;
+    }
+
+    public static int max(int[] numArr){
+        System.out.println("Maxof the array is: ");
+        return 0;
+    }
+}
--- Codes/Intermediate/NumberOfOccurences.java
+++ Codes/Intermediate/NumberOfOccurences.java
@@ -1,7 +1,25 @@
 package Codes.Intermediate;
+import java.util.Scanner;
 
 public class NumberOfOccurences {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         int[] numArray = ArrayUtility.inputArray();
+        System.out.println("Enter the number you want to find: ");
+        int num = input.nextInt();
+        int noOfOccs = occurences(numArray , num);
+        System.out.println("Your element was found " + noOfOccs + " times in the array.");
+    }
+
+    public static int occurences(int[] numArray , int num){
+        int occ = 0;
+        int i = 0;
+        while( i < numArray.length){
+            if (numArray[i] == num){
+                occ++;
+            }
+            i++;
+        }
+        return occ;
     }
 }
