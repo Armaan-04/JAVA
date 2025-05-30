edf5b8280162040fcb002994258597d6241bcc59
Armaan-04 <alamarmaan2004@gmail.com>
2025-05-30 12:03:47 +0530

    practice

---
+++ Codes/Intermediate/Course.java
@@ -0,0 +1,30 @@
+package Codes.Intermediate;
+
+public class Course {
+    String courseName;
+    int enrollments;
+    static int maxCapacity = 100;
+
+    String[] enrolledStudents;
+
+    Course(String courseName){
+        this.courseName = courseName;
+        this.enrollments = 0;
+        this.enrolledStudents = new String[maxCapacity];
+    }
+
+
+
+    static void setMaxCapacity(int maxCapacity){
+        Course.maxCapacity = maxCapacity;//when name is conflicting and it is a static variable..use class name to access.
+    }
+        void enrollStudent(String studentName){
+            enrolledStudents[enrollments] = studentName;
+            enrollments++;
+        }
+
+        void unEnrollStudent(String studentName){
+            System.out.println("Student removed.");
+            enrollments--;
+        }
+}
---
+++ Codes/Intermediate/SimpleCalculator.java
@@ -0,0 +1,23 @@
+package Codes.Intermediate;
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your first number: ");
+        int num1 = input.nextInt();
+        System.out.println("Enter your seocnd number: ");
+        int num2 = input.nextInt();
+        System.out.println("Now enter the operation: ");
+        String operation = input.next();
+
+        int result = switch (operation){
+            case "+" -> num1 + num2;
+            case "-" -> num1 - num2;
+            case "*" -> num1 * num2;
+            case "/" -> num1 / num2;
+            default -> -1;
+        };
+        System.out.println("Your answer is " + result);
+    }
+}
