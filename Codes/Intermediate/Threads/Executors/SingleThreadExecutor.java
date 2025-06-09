9903ab9961a889e3a47809f9bd3457413f26c4fb
Armaan-04 <alamarmaan2004@gmail.com>
2025-06-09 09:46:31 +0530

    Threads

---
+++ Codes/Intermediate/Threads/Executors/SingleThreadExecutor.java
@@ -0,0 +1,12 @@
+package Codes.Intermediate.Threads.Executors;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+
+public class SingleThreadExecutor {
+    public static void main(String[] args) {
+       try( ExecutorService service = Executors.newSingleThreadExecutor()){
+           PrintNumbers task = new PrintNumbers();
+           service.submit(task);
+       }
+    }
+}
