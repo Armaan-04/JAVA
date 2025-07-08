742e6f158b04c5762986217271249ffa80250a1f
Armaan Alam <alamarmaan2004@gmail.com>
2025-07-09 02:52:54 +0530

    Delete Introduction directory

--- Introduction/ClassFolder/shape2.java
+++
@@ -1,6 +0,0 @@
-package Introduction.ClassFolder;
-
-public interface shape2 {//every method in an interface is abstract.
-    void draw();
-    void printArea();
-}
--- Introduction/FirstCode.java
+++
@@ -1,7 +0,0 @@
-package Introduction;
-
-public class FirstCode{
-    public static void main(String[] args){
-        System.out.println("Hello World!");
-    }
-}
